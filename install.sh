a4234a5d428a898c9013d5dcafb66ad2f754e2fa
Étienne Malenfant <etiennemalenfant11@gmail.com>
2025-04-21 19:54:51 -0400

    Optimised install script

--- install.sh
+++ install.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -e
 
-echo "Extracting Boost..."
-tar -xzf libs/boost.tar.gz -C libs/
-echo "Extracting SFML..."
-tar -xzf libs/sfml.tar.gz -C libs/
+cd libs
 
+echo "Extracting Boost..."
+if [ ! -d boost ]; then
+    tar -xzf boost.tar.gz -C .
+else
+    echo "Boost already extracted."
+fi
+
+echo "Extracting SFML..."
+if [ ! -d sfml ]; then
+    tar -xzf sfml.tar.gz -C .
+else
+    echo "SFML already extracted."
+fi
+cd ..
 
 echo "Building TileRummy2D..."
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j$(nproc)
+cd ..
 
 echo "Copying data files..."
-cd ..
 mkdir -p bin
 mkdir -p bin/data
-cp -ru data bin/data
+cp -ru data/* bin/data
 cp build/TileRummy2D bin
 
 echo "Done. The game is in the bin directory."
